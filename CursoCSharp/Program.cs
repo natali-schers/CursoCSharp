66fe89c60d74e3181e18cd09f419324526a6a0b9
natali-schers <schersnatali@gmail.com>
2024-03-25 23:19:33 -0300

    [Aula17] Restrições e convenções para nomes

--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -1,12 +1,2 @@
-﻿sbyte n1 = 100; // Não aceita números maiores que 127 e pode ir até -128
-Console.WriteLine(n1);
-
-byte n2 = 255; // Não aceita números negativos e números maiores que 255
-Console.WriteLine(n2);
-
-// Overflow: quando um cálculo resulta em um número maior que o permitido pela variável
-n2++;
-Console.WriteLine(n2);
-
-int n3 = 2147483647;
-long n4 = 2147483648;
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
