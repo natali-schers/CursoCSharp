331339a1dd390ac8b8e0f66144ef1cf5274a6fb1
natali-schers <schersnatali@gmail.com>
2024-03-25 22:23:38 -0300

    [Aula16] - Tipos básicos de dado em C#

---
+++ Aula16/Program.cs
@@ -0,0 +1,25 @@
+﻿bool completo = false;
+Console.WriteLine(completo);
+
+char genero = 'F'; // Seu valor pode ser colocado em formato unicode
+Console.WriteLine(genero);
+
+float n1 = 4.5f; // É necessário incluir a letra f ápós o número, para que o C# entenda como float e não como double
+Console.WriteLine(n1);
+
+double n2 = 4.5;
+Console.WriteLine((n2));
+
+string nome = "Natali ";
+Console.WriteLine(nome);
+
+object obj1 = "Schers"; // Object é um tipo genérico
+object obj2 = 19.5;
+Console.WriteLine(obj1);
+Console.WriteLine(obj2);
+
+int n3 = int.MinValue; // Valor mínimo do tipo int
+Console.WriteLine(n3);
+
+int n4 = int.MaxValue;
+Console.WriteLine(n4); // Valor máximo do tipo int
