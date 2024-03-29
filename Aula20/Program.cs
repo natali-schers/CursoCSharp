66a0a2563b43ab9ee39187faf98c2849697af554
natali-schers <schersnatali@gmail.com>
2024-03-28 23:09:00 -0300

    [Aula20] - Conversão implícita e casting

---
+++ Aula20/Program.cs
@@ -0,0 +1,19 @@
+﻿// Conversão implícita entre tipos
+float x = 4.5f;
+double y = x;
+
+
+// Conversão explícita entre tipos (Casting)
+double a;
+int b;
+
+a = 5.1;
+b = (int)a;
+
+Console.WriteLine(b);
+
+int c = 5;
+int d = 2;
+
+double resultado = (double)c / d;
+Console.WriteLine(resultado);
