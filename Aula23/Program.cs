ed76c506b96c12fa21a81792b9a493992a6cc23f
natali-schers <schersnatali@gmail.com>
2024-03-29 23:16:43 -0300

    [Aula23] - Entrada de dados em C# - PARTE 2

---
+++ Aula23/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+
+int i = int.Parse(Console.ReadLine()); 
+Console.WriteLine("Você digitou: " + i);
+
+char ch = char.Parse(Console.ReadLine());
+Console.WriteLine("Você digitou: " + ch);
+
+double db = double.Parse(Console.ReadLine());
+Console.WriteLine("Você digitou: " + db);
+
+Console.WriteLine("Digite seu nome, sexo, idade e altura: ");
+string[] vet = Console.ReadLine().Split(' ');
+string nome = vet[0];
+char sexo = char.Parse(vet[1]);
+int idade = int.Parse(vet[2]);
+double altura = double.Parse(vet[3]);
+
+Console.WriteLine(nome);
+Console.WriteLine(sexo); 
+Console.WriteLine(idade);
+Console.WriteLine(altura);
---
+++ Exercicio2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+Console.WriteLine("Entre com seu nome completo: ");
+string nome = Console.ReadLine();
+
+Console.WriteLine("Quantos quartos tem na sua casa? ");
+int quartos = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Entre com o preço de um produto: ");
+double preco = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Entre com seu último nome, idade e altura (mesma linha): ");
+string dados = Console.ReadLine();
+string[] vet = dados.Split(' ');
+
+Console.WriteLine(nome);
+Console.WriteLine(quartos);
+Console.WriteLine(preco);
+Console.WriteLine(vet[0]);
+Console.WriteLine(vet[1]);
+Console.WriteLine(vet[2]);
