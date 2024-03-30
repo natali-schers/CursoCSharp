6b8a869795b83e1284eaef9424859ff6e561c1a8
natali-schers <schersnatali@gmail.com>
2024-03-29 22:48:03 -0300

    [Aula22] - Entrada de dados em C# - PARTE 1

---
+++ Aula22/Program.cs
@@ -0,0 +1,15 @@
+﻿// Console.ReadLine(): lê da entrada padrão até a quebra de linha e retorna os dados na forma de string.
+
+string frase = Console.ReadLine();
+string cor1 = Console.ReadLine();
+string cor2 = Console.ReadLine();
+string cor3 = Console.ReadLine();
+
+Console.WriteLine("Você digitou: ");
+Console.WriteLine(frase);
+Console.WriteLine(cor1);
+Console.WriteLine(cor2);
+Console.WriteLine(cor3);
+
+string[] vet = frase.Split(" ");
+Console.WriteLine(vet[0]);
