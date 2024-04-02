a9e1ccefa96c3a50d7f5a21889eeb36ef274184b
natali-schers <schersnatali@gmail.com>
2024-04-01 23:24:33 -0300

    [Aula26] - Operadores lógicos

---
+++ Aula26/Program.cs
@@ -0,0 +1,9 @@
+﻿// Ordem de procedência: ! && ||
+
+bool t1 = 4 != 5 && 2 > 3;
+bool t2 = 4 != 5 || 2 > 3;
+bool t3 = 4 != 5 && !(2 > 3);
+
+Console.WriteLine(t1);
+Console.WriteLine(t2);
+Console.WriteLine(t3);
