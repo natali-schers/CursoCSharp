848a3e53d6b4ab5a06933dedee88677e6460c9c8
natali-schers <schersnatali@gmail.com>
2024-04-01 23:15:27 -0300

    [Aula25] - Operadores comparativos

---
+++ Aula25/Program.cs
@@ -0,0 +1,18 @@
+﻿//Operadores comparativos
+int num = 10;
+
+bool t1 = num < 10;
+bool t2 = num > 10;
+bool t3 = num <= 10;
+bool t4 = num >= 5;
+
+Console.WriteLine(t1);
+Console.WriteLine(t2);
+Console.WriteLine(t3);
+Console.WriteLine(t4);
+
+bool t5 = num == 5;
+bool t6 = num != 5;
+
+Console.WriteLine(t5);
+Console.WriteLine(t6);
