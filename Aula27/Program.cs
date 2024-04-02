a783ca7f7b02a849a6dbad3fcbe1897f5704448f
natali-schers <schersnatali@gmail.com>
2024-04-01 23:34:35 -0300

    [Aula27] - Estrutura condicional (if-else)

---
+++ Aula27/Program.cs
@@ -0,0 +1,38 @@
+﻿// Estrutura condicional if simples 
+int num1 = 45;
+
+if (num1 >= 44)
+{
+    Console.WriteLine("True");
+}
+
+// Estrutura condicional if composta
+int num2 = 394;
+
+if (num2 == 990)
+{
+    Console.WriteLine("False");
+} else
+{
+    Console.WriteLine("True");
+}
+
+// Estrutura condicional if composta com encadeamentos
+int num3 = 6;
+
+if (num3 < 1)
+{
+    Console.WriteLine("False");
+}
+else if (num3 == 6)
+{
+    Console.WriteLine("True");
+}
+else if (num3 == 0)
+{
+    Console.WriteLine("False");
+}
+else
+{
+    Console.WriteLine("False");
+}
