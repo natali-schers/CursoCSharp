bec3f9ccdbc5c915ae5bde1fe7fa3fa3a10fc8cd
natali-schers <schersnatali@gmail.com>
2024-04-02 22:40:12 -0300

    [Aula30] - Funções (sintaxe)

---
+++ Aula30/Program.cs
@@ -0,0 +1,22 @@
+﻿int n1 = int.Parse(Console.ReadLine());
+int n2 = int.Parse(Console.ReadLine());
+int n3 = int.Parse(Console.ReadLine());
+
+static int Maior(int n1, int n2, int n3)
+{
+    int maior;
+
+	if (n1 > n2 && n1 > n3)
+	{
+		maior = n1;
+	} else if (n2 > n1 && n2 > n3) {
+		maior = n2;
+	} else
+	{
+		maior = n3;
+	}
+
+	return maior;
+}
+
+Console.WriteLine(Maior(n1, n2, n3));
