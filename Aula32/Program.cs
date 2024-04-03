6da66217696207a9cec06995f51de536da57a342
natali-schers <schersnatali@gmail.com>
2024-04-02 22:49:28 -0300

    [Aula32] - Estrutura repetitiva enquanto (while)

---
+++ Aula32/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Digite um número inteiro: ");
+int num = int.Parse(Console.ReadLine());
+
+while (num > 0)
+{
+    double raiz = Math.Sqrt(num);
+    Console.WriteLine(raiz);
+
+    Console.WriteLine("Digite outro número inteiro: ");
+    num = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("Entrada inválida!");
