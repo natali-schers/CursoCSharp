e3a554d8058ca77f2eb4d9a0f2174e4d0263b88f
natali-schers <schersnatali@gmail.com>
2024-04-09 23:13:59 -0300

    [Aula34] - Estrutura repetitiva para (for)

---
+++ Aula34/Program.cs
@@ -0,0 +1,16 @@
+﻿// Início (executa somente na primeira vez); condição; incremento
+
+Console.Write("Quantos números inteiros você quer digitar? ");
+int qntd = int.Parse(Console.ReadLine());
+
+int soma = 0;
+
+for (int i = 1; i <= qntd; i++)
+{
+    Console.Write($"Valor: #{i}: ");
+    int valor = int.Parse(Console.ReadLine());
+
+    soma += valor;
+}
+
+Console.WriteLine("Soma = " + soma);
