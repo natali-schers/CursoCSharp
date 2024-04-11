20777ec30ec619f67cc26ba557d2ef5ffd3d83bb
natali-schers <schersnatali@gmail.com>
2024-04-11 02:11:33 -0300

    [Aula41] - Criando um método para obtermos os benefícios de reaproveitamento e delegação

---
+++ Aula41/Program.cs
@@ -0,0 +1,35 @@
+﻿using Aula41;
+using System.Globalization;
+
+Triangulo x = new Triangulo();
+
+Console.WriteLine("Entre com as medidas do triângulo X: ");
+x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+double xArea = x.CalculaArea();
+
+Triangulo y = new Triangulo();
+
+Console.WriteLine("Entre com as medidas do triângulo X: ");
+y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+double yArea = y.CalculaArea();
+
+Console.WriteLine(" ");
+
+if (xArea > yArea)
+{
+    Console.WriteLine("Maior área: X");
+}
+else if (yArea > xArea)
+{
+    Console.WriteLine("Maior área: Y");
+}
+else
+{
+    Console.WriteLine("Áreas iguais!");
+}
---
+++ Aula41/Triangulo.cs
@@ -0,0 +1,18 @@
+﻿namespace Aula41
+{
+    class Triangulo
+    {
+        public double A;
+        public double B;
+        public double C;
+
+        public double CalculaArea()
+        {
+            double p = (A + B + C) / 2;
+
+            double Area = Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+
+            return Area;
+        }
+    }
+}
