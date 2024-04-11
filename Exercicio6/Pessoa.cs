53c763b552c0deb585d7d965ecba69e490a67ac1
natali-schers <schersnatali@gmail.com>
2024-04-11 01:30:29 -0300

    [Aula40] - Exercícios de fixação

---
+++ Exercicio6/Pessoa.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio6
+{
+    class Pessoa
+    {
+        public string Nome;
+
+        public double Salario;
+    }
+}
---
+++ Exercicio6/Program.cs
@@ -0,0 +1,25 @@
+﻿using Exercicio6;
+
+Pessoa pessoa1 = new Pessoa();
+
+Console.Write("Digite o nome da primeira pessoa: ");
+pessoa1.Nome = Console.ReadLine();
+
+Console.Write("Digite o salário da primeira pessoa: R$");
+pessoa1.Salario = double.Parse(Console.ReadLine());
+
+Console.WriteLine(" ");
+
+Pessoa pessoa2 = new Pessoa();
+
+Console.Write("Digite o nome da segunda pessoa: ");
+pessoa2.Nome = Console.ReadLine();
+
+Console.Write("Digite o salário da segunda pessoa: R$");
+pessoa2.Salario = double.Parse(Console.ReadLine());
+
+double media = (pessoa1.Salario + pessoa2.Salario) / 2.00;
+
+Console.WriteLine(" ");
+
+Console.WriteLine("A média dos salários é de: R${0}", media);
