ec08f32d3d9c46fc38fb930d70fab06021d4d3d2
natali-schers <schersnatali@gmail.com>
2024-04-11 22:01:17 -0300

    [Aula42] - Object e ToString

---
+++ Aula42/Produto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula42
+{
+    class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotalEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        public override string ToString()
+        {
+            return "Produto: " + Nome + ". Valor unitário R$" + Preco.ToString("F2", CultureInfo.InvariantCulture) + ". Estoque: " + Quantidade + " unidades. Valor total em estoque: R$" + ValorTotalEstoque().ToString("F2", CultureInfo.InvariantCulture) + ".";
+        }
+    }
+}
---
+++ Aula42/Program.cs
@@ -0,0 +1,16 @@
+﻿using Aula42;
+
+Console.WriteLine("Entre os dados do produto: ");
+
+Produto produto = new Produto();
+
+Console.Write("Nome: ");
+produto.Nome = Console.ReadLine();
+
+Console.Write("Preço: R$");
+produto.Preco = double.Parse(Console.ReadLine());
+
+Console.Write("Estoque: ");
+produto.Quantidade = int.Parse(Console.ReadLine());
+
+Console.WriteLine(produto);
