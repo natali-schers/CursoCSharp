02e27b1786f212f364aaa0020fd22e65863c0e0d
natali-schers <schersnatali@gmail.com>
2024-04-14 12:20:35 -0300

    [Aula45] - Exercícios de fixação

---
+++ Exercicio7/Funcionario.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio7
+{
+    class Funcionario
+    {
+        public string Nome;
+        public double SalarioBruto;
+        public double Imposto;
+
+        public double SalarioLiquido()
+        {
+            return SalarioBruto - Imposto;
+        }
+
+        public void AumentarSalario(double porcentagem)
+        {
+            SalarioBruto += (SalarioBruto * porcentagem / 100.0);
+        }
+
+        public override string ToString()
+        {
+            return "Funcionário: " + Nome + ". Salário: R$" + SalarioLiquido() + ".";
+        }
+    }
+}
---
+++ Exercicio7/Program.cs
@@ -0,0 +1,24 @@
+﻿using Exercicio7;
+
+Funcionario funcionario = new Funcionario();
+
+Console.Write("Digite o nome do funcionário: ");
+funcionario.Nome = Console.ReadLine();
+
+Console.Write("Digite o valor do salário bruto: R$");
+funcionario.SalarioBruto = double.Parse(Console.ReadLine());
+
+Console.Write("Digite o valor do imposto: R$");
+funcionario.Imposto = double.Parse(Console.ReadLine());
+
+Console.WriteLine(" ");
+Console.WriteLine(funcionario);
+
+Console.WriteLine(" ");
+Console.Write("Digite a porcentagem para aumentar o salário: ");
+double porcentagem = double.Parse(Console.ReadLine());
+
+funcionario.AumentarSalario(porcentagem);
+
+Console.WriteLine(" ");
+Console.WriteLine("Dados atualizados: " + funcionario);
