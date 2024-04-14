29bc9eaccfd87a93c1a78ff4e52cb52369d87101
natali-schers <schersnatali@gmail.com>
2024-04-14 20:23:47 -0300

    [Aula46] - Membros estáticos - PARTE 1

---
+++ Aula46/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+
+namespace Aula46
+{
+    class Program
+    {
+        static double Pi = 3.14;
+
+        static void Main(string[] args)
+        {
+            Console.Write("Entre com o valor do raio: ");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double circ = Circunferencia(raio);
+            double vol = Volume(raio);
+
+            Console.WriteLine("Valor de Pi: " + Pi);
+            Console.WriteLine("Circunferência: " + circ.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Volume: " + vol.ToString("F2", CultureInfo.InvariantCulture));
+        }
+
+        static double Circunferencia(double raio)
+        {
+            return 2.0 * Pi * raio;
+        }
+
+        static double Volume(double raio)
+        {
+            return 4.0 / 3.0 * Pi * Math.Pow(raio, 3);
+        }
+    }
+}
