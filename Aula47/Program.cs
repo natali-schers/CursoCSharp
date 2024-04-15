ba7695ef23c8a3e9d8fce88630db02f7ba9599a5
natali-schers <schersnatali@gmail.com>
2024-04-14 21:05:16 -0300

    [Aula47] - Membros estáticos - PARTE 2

---
+++ Aula47/Program.cs
@@ -0,0 +1,12 @@
+﻿using Aula47;
+using System.Globalization;
+
+Console.Write("Entre com o valor do raio: ");
+double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+double circ = Calculadora.Circunferencia(raio);
+double vol = Calculadora.Volume(raio);
+
+Console.WriteLine("Valor de Pi: " + Calculadora.Pi);
+Console.WriteLine("Circunferência: " + circ.ToString("F2", CultureInfo.InvariantCulture));
+Console.WriteLine("Volume: " + vol.ToString("F2", CultureInfo.InvariantCulture));
