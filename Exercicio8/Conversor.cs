d0b059b656e0aa1f097d1c592b4c5300c030d47f
natali-schers <schersnatali@gmail.com>
2024-04-15 23:50:46 -0300

    [Aula48] - Exercícios de fixação

---
+++ Exercicio8/Conversor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio8
+{
+    class Conversor
+    {
+        public static double Imposto = 0.06;
+
+        public static double TotalEmReais(double cotacao, double quantidade)
+        {
+            double total = cotacao * quantidade;
+            double totalComImposto = total + total * Imposto;
+
+            return totalComImposto;
+        }
+    }
+}
---
+++ Exercicio8/Program.cs
@@ -0,0 +1,9 @@
+﻿using Exercicio8;
+
+Console.Write("Qual é a cotação do dólar? R$");
+double cotacao = double.Parse(Console.ReadLine());
+
+Console.Write("Quantos dólares você pretende comprar? U$");
+double quantidade = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Valor a ser pago em reais: R$" + Conversor.TotalEmReais(cotacao, quantidade).ToString("F2"));
