a61c15e7be627bf989db529b504abdb231138f9a
natali-schers <schersnatali@gmail.com>
2024-04-19 23:07:16 -0300

    [Aula55] - Encapsulamento

---
+++ Aula55/Produto.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula55
+{
+    class Produto
+    {
+        //Padrão de nomes para atributos privados: _atributo
+        private string _nome;
+        private double _preco;
+        private int _quantidade;
+        public Produto()
+        {
+        }
+
+        public Produto(string nome, double preco, int quantidade)
+        {
+            _nome = nome;
+            _preco = preco;
+            _quantidade = quantidade;
+        }
+
+        public string GetNome()
+        {
+            return _nome;
+        }
+
+        public void SetNome(string nome)
+        {
+            if (string.IsNullOrEmpty(nome)) 
+            {
+                _nome = nome;
+            }
+        }
+
+        public double GetPreco()
+        {
+            return _preco;
+        }
+
+        public double GetQuantidade()
+        {
+            return _quantidade;
+        }
+
+        public double ValorTotalEstoque()
+        {
+            return _preco * _quantidade;
+        }
+
+        public void AdicionarEstoque(int estoque)
+        {
+            _quantidade += estoque;
+        }
+
+        public void RemoverEstoque(int estoque)
+        {
+            _quantidade -= estoque;
+        }
+
+        public override string ToString()
+        {
+            return "Produto: " + _nome + ". Valor unitário R$" + _preco.ToString("F2") + ". Estoque: " + _quantidade + " unidades. Valor total em estoque: R$" + ValorTotalEstoque().ToString("F2") + ".";
+        }
+    }
+}
---
+++ Aula55/Program.cs
@@ -0,0 +1,15 @@
+﻿// Encapsulamento: esconde detalhes de implementação de um componente, expondo apenas operações seguras que o mantenham em um estado consistente
+// Exemplo: rádio - operações básica: tocar, pausar, avançar e voltar música. Operações internas ficam fora do alcance do usuário.
+
+// Todo atributo será definido como private
+// São implementados get(pegar) e set(definir) para cada atributo
+
+using Aula55;
+
+Produto produto = new Produto("TV", 900.00, 10);
+
+Console.WriteLine(produto.GetNome());
+
+produto.SetNome("TV 4k");
+
+Console.WriteLine(produto.GetNome());
