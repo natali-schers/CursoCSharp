27b11c09351c6630f223944bfbb8f59151fd1db1
natali-schers <schersnatali@gmail.com>
2024-05-01 15:40:25 -0300

    [Aula56] - Properties

---
+++ Aula56/Produto.cs
@@ -0,0 +1,30 @@
+﻿
+namespace Aula56
+{
+    class Produto
+    {        
+        private string _nome;
+        private double _preco;
+        private int _quantidade;
+
+        public string Nome { 
+            get { return _nome; }
+            set {
+                if (string.IsNullOrEmpty(value))
+                {
+                    _nome = value;
+                }
+            }
+        }
+
+        public double Preco
+        {
+            get { return _preco; }
+        }
+
+        public int Quantidade
+        {
+            get { return _quantidade; }
+        }
+    }
+}
---
+++ Aula56/Program.cs
@@ -0,0 +1,2 @@
+﻿// As propriedades podem ser usadas como se fossem atributos públicos,
+// permitindo que os dados sejam acessados com segurança e facilidade
