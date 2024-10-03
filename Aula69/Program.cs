952f76b925cf4608f84688f9d8b9db4d646b9353
Natali Schers <schersnatali@gmail.com>
2024-10-02 22:04:12 -0300

    [Aula69] - Vetores pt.1

---
+++ Aula69/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Escolha o tamanho do vetor: ");
+        int n = int.Parse(Console.ReadLine());
+
+        double[] vetor = new double[n];
+        double soma = 0.0;
+
+        Console.WriteLine("");
+        Console.WriteLine("Digite os valores do vetor: ");
+        for (int i = 0; i < n; i++)
+        {
+            vetor[i] = double.Parse(Console.ReadLine());
+            soma += vetor[i];
+        }
+
+        double media = soma/ n;
+
+        Console.WriteLine("");
+        Console.WriteLine("A média dos valores é: " + media.ToString("F2"));
+    }
+}
